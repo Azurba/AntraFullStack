5f8ab56d27b8f91b62b1fd51f3df8a1325b8bc66
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-02-03 10:53:04 -0700

    Add files via upload

---
+++ Assingment7/Assingment7/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment7
+{
+    interface IRepository<T>
+    {
+        void Add(T item);
+        void Remove(T item);
+        void Save();
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        
+    }
+}
---
+++ Assingment7/Assingment7/MyList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment7
+{
+    class MyList<T>
+    {
+        private T[] data;
+        private int size = 0;
+        private int capacity;
+
+        public MyList(int capacity = 5)
+        {
+            this.capacity = capacity;
+            data = new T[capacity];
+        }
+
+        public void add(T element)
+        {
+            if (size == capacity)
+            {
+                Console.WriteLine("Can't add it");
+            }
+
+            data[size] = element;
+            size++;
+        }
+
+        //same as delete at?
+        public T remove(int index)
+        {
+            T returnedElement = default(T);
+            for (int i = index; i < size - 1; i++)
+            {
+                if (!(data[i].Equals(null))) {
+                    data[i] = returnedElement;
+                }
+                data[i] = data[i + 1];
+
+            }
+
+            data[size - 1] = default(T);
+            size--;
+            return returnedElement;
+        }
+
+        public bool Contains(T value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                T currentValue = data[i];
+                if (currentValue.Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void clear() {
+            data = new T[capacity];
+            size = 0;
+        }
+
+        public void insertAt(T element, int index)
+        {
+            if (size == capacity)
+            {
+                Console.WriteLine("Can't add it");
+            }
+
+            for (int i = size; i > index; i--)
+            {
+                data[i] = data[i - 1];
+            }
+
+            data[index] = element;
+            size++;
+        }
+
+        public void DeleteAt(int index)
+        {
+            for (int i = index; i < size - 1; i++)
+            {
+                data[i] = data[i + 1];
+            }
+
+            data[size - 1] = default(T);
+            size--;
+
+        }
+
+        public int find(T element) {
+            for (int i = 0; i <= capacity; i++)
+            {
+                data[i].Equals(element);
+                return i;
+            }
+            return -1;
+
+        }
+    }
+}
