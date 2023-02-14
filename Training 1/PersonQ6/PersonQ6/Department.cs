5493d1713dc307c6b8b296fd72c65df0b36e1d83
Azurba <joaoh.pimenta.giudice@gmail.com>
2023-02-14 09:09:34 -0700

    add folders

---
+++ Training 1/PersonQ6/PersonQ6/Department.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonQ6
+{
+    class Department : IDepartmentService
+    {
+        protected Instructor head { get; set; }
+        //value of the budget and start and end date for the school year
+        protected Dictionary<double, DateTime> budget = new Dictionary<double, DateTime>();
+
+        protected string departmentName { get; set; } = "DefaultName";
+
+        Department() {
+            List<string> defaultAddress = new List<string>();
+            defaultAddress.Add("None");
+            head = new Instructor(-1, -1, defaultAddress, this.departmentName, true, -1);
+
+            budget.Add(0, DateTime.Now);
+
+        }
+
+        Department(Instructor head, Dictionary<double, DateTime> budget) {
+            this.head = head;
+            this.budget = budget;
+        }
+
+        public Dictionary<double, DateTime> getBudget() {
+            return budget;
+        }
+
+    }
+}
---
+++ Training 1/PersonQ6/PersonQ6/Instructor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonQ6
+{
+    class Instructor : Person, IInstructorService
+    {
+        protected string department { get; set; }
+        protected bool head { get; set; }
+        protected int yearsExperience { get; set; }
+
+
+        public Instructor() {
+            this.department = "Default department";
+            this.head = false;
+            yearsExperience = 0;
+
+        }
+        public Instructor(int age, decimal salary, List<string> address, string department, bool isHead, int yearsExperience)
+        {
+            this.age = age;
+            this.salary = salary;
+            this.address = address;
+            this.department= department;
+            this.head= isHead;
+            this.yearsExperience= yearsExperience;
+        }
+
+        public decimal calculateExtraSalary(int yearsExperience) {
+            return salary + (yearsExperience * 500);
+        }
+
+    }
+}
