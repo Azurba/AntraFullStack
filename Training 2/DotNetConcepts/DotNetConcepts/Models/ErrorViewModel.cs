86e8577af6818be559b7946539fe7af92864fa5e
Azurba <joaoh.pimenta.giudice@gmail.com>
2023-02-19 12:28:04 -0700

    .

--- Training 2/DotNetConcepts/DotNetConcepts/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DotNetConcepts.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Training 2/assignment1/index.js
+++
@@ -1,4 +0,0 @@
-function emailSent(){
-    document.getElementById("sent").style.color='black';
-    document.getElementById("thankyou").style.color='black';
-  }
