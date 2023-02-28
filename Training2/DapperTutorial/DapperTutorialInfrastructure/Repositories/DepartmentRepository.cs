0e8528bb8c872b8b6429825a20bc3a0d57a12c17
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-02-28 13:26:12 -0700

    Add dapper tutorial

---
+++ Training2/DapperTutorial/DapperTutorialInfrastructure/Repositories/DepartmentRepository.cs
@@ -0,0 +1,55 @@
+﻿using DapperTutorialCore.Contracts.Repositories;
+using DapperTutorialCore.Entities;
+using DapperTutorialInfrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperTutorialInfrastructure.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        DapperDBContext dbContext;
+        public DepartmentRepository()
+        { 
+            //This invokes our connection
+            dbContext= new DapperDBContext();
+            
+        }
+        public int DeleteById(int id)
+        {
+
+            using (var conn = dbContext.GetConnection()) 
+            {
+                conn.Open();
+                return conn.Execute("Delete From Department Where Id=@Id", new {deptId=id});
+            };
+            
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            using (var conn = dbContext.GetConnection())
+            {
+                return conn.Query<Department>("Select Id, name, Location From Department");
+            };
+        }
+
+        public Department GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Task obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Task obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
